848cbba40bf0b14718c36c443414ccd66a7a734f
danielfc <daniel.felicissimo@gmail.com>
2016-10-27 12:05:22 -0700

    Remove log

--- popup.js
+++ popup.js
@@ -3,13 +3,7 @@ var rates = localStorage.getItem("rates");
 if (rates) {
     rates = JSON.parse(rates);
 
-    chrome.extension.getBackgroundPage().console.log(rates);
-
     document.getElementById("fox-rate").innerHTML = rates.fox;
     document.getElementById("loc-rate").innerHTML = rates.loc;
     document.getElementById("b2u-rate").innerHTML = rates.b2u;
-} else {
-    document.getElementById("fox-rate").innerHTML = "...";
-    document.getElementById("loc-rate").innerHTML = "...";
-    document.getElementById("b2u-rate").innerHTML = "...";
 }
